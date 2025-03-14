2301a8f6b100c0235339e4123d28a71d83803a2d
El Groovy <elisagroup@gmail.com>
2025-03-15 02:21:41 +0400

    Implemented mint-based team airdrop; Fixed some issues with the reward distributor script

---
+++ mint/airdrop.js
@@ -0,0 +1,79 @@
+
+import { Connection, PublicKey, clusterApiUrl, SystemProgram } from '@solana/web3.js';
+import { mintToChecked, getOrCreateAssociatedTokenAccount, TOKEN_2022_PROGRAM_ID } from '@solana/spl-token';
+
+import { TokenConfig } from './token-config.js';
+import { Constants } from '../constants.js';
+
+import { loadKeypairFromFile } from "../keypair-utils.js";
+
+import fs from 'fs';
+import csv from 'csv-parser'
+
+
+const TOTAL_TOKENS_TO_DISTRIBUTE = Math.floor(1_000_000_000 * 0.7); // 70%
+
+const csvFilePath = '/home/elisa/sol-rewards/mint/airdrop.csv';
+let totalContribution = 0;
+let contributions = [];
+
+const connection = new Connection(clusterApiUrl(TokenConfig.kSolanaNetwork), "confirmed");
+const mintPubkey = new PublicKey(Constants.kTokenMintPubkey);
+
+// Load the owner wallet keypair
+let ownerKeypair = null;
+try {
+    ownerKeypair = await loadKeypairFromFile(TokenConfig.kOwnerWalletKeyFile);
+} catch (error) {
+    console.error("Failed to load the owner keypair:", error);
+    throw error;
+}
+
+try {
+    fs.createReadStream(csvFilePath)
+        .pipe(csv())
+        .on('data', (row) => {
+            const walletAddress = row.walletAddress;
+            const contribution = parseFloat(row.contribution);
+            contributions.push({ walletAddress, contribution });
+            totalContribution += contribution;
+        })
+        .on('end', async () => {
+            console.log('CSV file successfully processed. Starting airdrop...');
+            for (const { walletAddress, contribution } of contributions) {
+                const share = contribution / totalContribution;
+                const amountToMint = BigInt(Math.floor(share * TOTAL_TOKENS_TO_DISTRIBUTE)) * BigInt(10 ** TokenConfig.kDecimals);
+    
+                const recipientPublicKey = new PublicKey(walletAddress);
+
+                // Get the token account of the recipientPublicKey. If it does not exist, create it
+                const destinationTokenAccount = await getOrCreateAssociatedTokenAccount(
+                    connection,
+                    ownerKeypair,
+                    mintPubkey,
+                    recipientPublicKey,
+                    true,
+                    "finalized",
+                    { commitment: "finalized" }, // confirmation options
+                    TOKEN_2022_PROGRAM_ID,
+                );
+    
+                let txhash = await mintToChecked(
+                    connection,
+                    ownerKeypair,
+                    mintPubkey,
+                    destinationTokenAccount.address,
+                    ownerKeypair.publicKey,
+                    amountToMint,
+                    TokenConfig.kDecimals,
+                    [],
+                    { commitment: "finalized" }, // confirmation options
+                    TOKEN_2022_PROGRAM_ID
+                );
+                console.log(`Sent to ${walletAddress}, txhash: ${txhash}`);
+            }
+            console.log("Airdop finished.");
+        });
+} catch (error) {
+    console.error(error);
+}
