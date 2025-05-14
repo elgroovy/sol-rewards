c9e3297a62e87068dbe9ca956f2698c1f3b64ea0
El Groovy <elisagroup@gmail.com>
2025-05-14 04:12:11 +0400

    Updated Helium RPC url

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 
 export const Constants = {
     kSolanaNetwork : "mainnet-beta",
-    kHeliusRPCEndpoint : "https://mainnet.helius-rpc.com/?api-key=fd4ac0ba-d60e-4b97-b121-22141efc9c16",
+    kHeliusRPCEndpoint : "https://mainnet.helius-rpc.com/?api-key=9bddc7cc-e4cf-4933-b10c-12e6fedc9f10", //"https://mainnet.helius-rpc.com/?api-key=fd4ac0ba-d60e-4b97-b121-22141efc9c16",
     kHeliusDevnetRPCEndpoint : "https://devnet.helius-rpc.com/?api-key=fd4ac0ba-d60e-4b97-b121-22141efc9c16",
 
     kBackendUrl: "http://ec2-3-121-42-215.eu-central-1.compute.amazonaws.com:3000", // "http://localhost:3000"
